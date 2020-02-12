0ba4699811ea744294795b7e52d574f7faa78a7e
Ken Kousen <ken.kousen@kousenit.com>
2020-02-11 19:11:26 -0600

    Add logging aspect

---
+++ src/main/java/com/churchmutual/restclient/aspects/LoggingAspect.java
@@ -0,0 +1,19 @@
+package com.churchmutual.restclient.aspects;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LoggingAspect {
+    private Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
+
+    @Before("execution(* com.churchmutual.restclient.*.*.*(..))")
+    public void logMethodCalls(JoinPoint joinPoint) {
+        logger.info("About to execute " + joinPoint.getSignature());
+    }
+}
